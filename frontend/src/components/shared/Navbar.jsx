67534405105f763fc1f3bd6f37ba62d81e4d18d3
Abhay Yadav <162044736+abhayyadav9@users.noreply.github.com>
2025-02-25 01:24:54 +0530

    Update Navbar.jsx

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
   const logoutHandler = async () => {
     try {
       dispatch(setLoading(true));
-      const res = await axios.post("https://jobhunt-bskd.onrender.com/api/v1/user/logout", {
+      const res = await axios.post("https://jobhunt-backend-vtaa.onrender.com/api/v1/user/logout", {
         withCredentials: true,
       });
 
